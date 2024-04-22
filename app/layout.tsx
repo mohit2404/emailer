dad50ada7a2c350c4954035b19163e4cab1f2e09
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2024-04-23 00:59:04 +0530

    add opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,34 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Emailer App',
-  description: 'Send Bulk Emails',
-}
+  title: "Emailer App",
+  description: "Send Bulk Emails",
+  openGraph: {
+    type: "website",
+    url: "https://example.com",
+    title: "My Website",
+    description: "My Website Description",
+    siteName: "My Website",
+    images: [
+      {
+        url: "https://staybook.in/_next/image?url=https%3A%2F%2Fimages.staybook.in%2FJagat-Niwas-Palace-Hotel%2FJagat-Niwas-Palace-Hotel-deluxe-room-4.webp&w=1920&q=75",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
